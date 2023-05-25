cac99a6423aadf787b2a0504035bcdb6b3c56df0
EftekharAlam2 <hinsouvlmare@gmail.com>
2023-05-25 22:53:17 +0600

    NavBar added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../SharedPage/Footer";
+import NavBar from "../SharedPage/NavBar";
 
 const Main = () => {
   return (
     <div>
+      <NavBar></NavBar>
       <Outlet></Outlet>
       <Footer></Footer>
     </div>
