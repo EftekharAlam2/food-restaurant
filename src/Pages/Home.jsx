1456c76f7edcbbca3837ea9171bccc1fde762f53
EftekharAlam2 <hinsouvlmare@gmail.com>
2023-05-27 08:31:59 +0600

    title edit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,10 +3,14 @@ import Category from "./Category";
 import Featured from "./Featured";
 import PopularMenu from "./PopularMenu";
 import Testimonials from "./Testimonials";
+import { Helmet } from "react-helmet-async";
 
 const Home = () => {
   return (
     <div>
+      <Helmet>
+        <title>Food Restaurant | Home</title>
+      </Helmet>
       <Banner></Banner>
       <Category></Category>
       <PopularMenu></PopularMenu>
