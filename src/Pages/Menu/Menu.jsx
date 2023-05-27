e69e7d0ef03486cb38811b17364c69c637097b07
EftekharAlam2 <hinsouvlmare@gmail.com>
2023-05-27 08:58:55 +0600

    menu page cover section added

---
+++ src/Pages/Menu/Menu.jsx
@@ -0,0 +1,20 @@
+import { Helmet } from "react-helmet-async";
+import Cover from "../../SharedPage/Cover";
+import menuImg from "../../assets/menu/menu-bg.jpg";
+import soupImg from "../../assets/menu/soup-bg.jpg";
+import saladImg from "../../assets/menu/salad-bg.jpg";
+import pizzaImg from "../../assets/menu/pizza-bg.jpg";
+import dessertImg from "../../assets/menu/dessert-bg.jpeg";
+
+const Menu = () => {
+  return (
+    <div>
+      <Helmet>
+        <title>Food Restaurant | Menu</title>
+      </Helmet>
+      <Cover img={menuImg} title="our menu"></Cover>
+    </div>
+  );
+};
+
+export default Menu;
