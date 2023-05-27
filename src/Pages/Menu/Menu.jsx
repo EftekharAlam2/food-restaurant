317e8490aa9f239137fcc25f08fee83b8a29d780
EftekharAlam2 <hinsouvlmare@gmail.com>
2023-05-27 09:10:13 +0600

    hook added

--- src/Pages/Menu/Menu.jsx
+++ src/Pages/Menu/Menu.jsx
@@ -1,6 +1,7 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../../SharedPage/Cover";
 import menuImg from "../../assets/menu/menu-bg.jpg";
+/* eslint-disable no-unused-vars */
 import soupImg from "../../assets/menu/soup-bg.jpg";
 import saladImg from "../../assets/menu/salad-bg.jpg";
 import pizzaImg from "../../assets/menu/pizza-bg.jpg";
